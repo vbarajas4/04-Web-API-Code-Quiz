1ff2b2672753398a876c67f66c7cc02ad2a13437
Vanessa Maldonado <vbarajas4@gmail.com>
2021-04-03 04:14:21 -0700

    added highscore and javascript almost complete

--- script.js
+++ script.js
@@ -1,54 +1,151 @@
+//variables from the DOM
+var startMenuEl = document.querySelector("#homepage");
+var questionArrayEl = document.querySelector("#questionArray");
+var questionsEl = document.getElementById("questions");
+var choicesEl = document.querySelector("#choices");
+var startButton = document.getElementById("start-button");
+var timerEl = document.getElementById("timer");
 
-
-
-
-
-
-
-
-
-let question = [
+//variables that keep track of the quiz as is active.
+var timerLeft = 75;
+var timerInterval;
+var quizIndex = 0;
+var hasQuizStarted = false;
+var question = [
     {
-        question: "Commonly used data types DO NOT include:",
-        choice1: "string",
-        choice2: "booleans",
-        choice3: "alerts",
-        choice4: "numbers",
-        answer: 3
+      title: "Commonly used data types DO NOT include:",
+      choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+      answer: "Alerts"
     },
     {
-        question: "The condition in an if/else statement is enclosed within ____",
-        choice1: "quotes",
-        choice2: "curly brackets",
-        choice3: "parentheses",
-        choice4: "square brackets",
-        answer: 3
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["Quotes", "Curly brackets", "Parentheses", "Square brackets"],
+      answer: "Parentheses"
     },
     {
-        question: "Arrays in JavaScript can be used to store ____.",
-        choice1: "numbers and strings",
-        choice2: "other arrays",
-        choice3: "booleans",
-        choice4: "All of the above",
-        answer: 4
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
+      answer: "All of the above"
     },
     {
-        question: "String values must be enclosed within ____ when being assigned to variables.",
-        choice1: "commas",
-        choice2: "curly brackets",
-        choice3: "quotes",
-        choice4: "parentheses",
-        answer: 1
+      title: "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["Commas", "Curly brackets", "Quotes", "Parentheses"],
+      answer: "Quotes"
+    },
+    {
+      title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "Terminal / Bash", "For loops", "Console.log"],
+      answer: "Console.log"
     }
-    {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choice1: "JavaScript",
-        choice2: "terminal / bash",
-        choice3: "for loops",
-        choice4: "console.log",
-        answer: 4
+  ];
+
+  //variables for end of the game
+
+var highScoreDisplay = document.querySelector("#highscore-display");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+var submitButton = document.querySelector("#submit-initials");
+
+var hasQuizEnded = false;
+var quizOverEl = document.querySelector("#quiz-over");
+var initials = '';
+var score = 0;
+var highscoreDisplay = JSON.parse(localStorage.getItem("highscore")) || [];
+
+
+//when clicking the button the following function need to be executed to start the quiz, timer, and questions
+
+startButton.addEventListener("click", function() {
+    startTime();
+    startQuiz();
+    startQuestion();
+})
+//start timer and show how much time is left before timer runs out and officially ending the quiz. 
+function startTime() {
+    timerInterval = setInterval(function() {
+        if (timerLeft <= 0) {
+            clearInterval(timerInterval);
+            endQuiz();
+        }
+        timerLeft--;
+        timerEl.textContent = "Timer: " + timerLeft;
+    }, 1000)
+}  
+// hide homepage instruction and start button and show the questions area
+function startQuiz() {
+    hasQuizStarted = true;
+    startMenuEl.setAttribute("class", "hide");
+    questionArrayEl.setAttribute("class", "show");
+    startQuestion();
+}
+//updating title with current questions from array and then clearing out any old choices from prior questions.
+function startQuestion() {
+    questionsEl.textContent = question[quizIndex].title;
+    choicesEl.innerHTML = '';
+    //looping each question, creating a button and adding a click event listener for each choice and display on page.
+    for (var i = 0; i < question[quizIndex].choices.length; i++) {
+        var button = document.createElement("button");
+        button.textContent = question[quizIndex].choices[i];
+        button.onclick = checkAnswer;
+        choicesEl.appendChild(button)
     }
-]
+}
+
+function checkAnswer () {
+
+    // check if user guessed wrong
+    if (this.textContent !== questions[quizIndex].answer) {
+      // subtract time
+      time -= 10;
+  
+      if (time < 0) {
+        time = 0;
+      }
+  
+      // display new time on page
+      timerEl.textContent = timer;
+  
+  
+      feedbackEl.textContent = "Wrong!";
+    } else {
+
+      feedbackEl.textContent = "Correct!";
+    }
+  
+    // display if answer was wrong or correct on page 
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "hide-feedback");
+    }, 1000);
+  
+    // move to next question
+    quizIndex++;
+  
+    // check if we've run out of questions
+    if (quizIndex === questions.length) {
+      endQuiz();
+      getInitials()
+    } else {
+      startQuestion();
+    }                      
+  }
+
+  // end the quiz 
+  function quizEnd() {
+    // stop timer
+    clearInterval(timerInterval);
+  
+    // show end screen
+    var highscoreSectionEl = document.querySelector("#highscore-section");
+    highscoreSectionEl.setAttribute("class", "show");
+  
+    // show final score
+    var finalScoreEl = document.querySelector("#final-score");
+    finalScoreEl.textContent = time;
+  
+    // hide questions section
+    quizScreen.setAttribute("class", "hide");
+  }
 
 
 
@@ -56,3 +153,91 @@ let question = [
 
 
 
+/*var highscoresContainer.innerHtml = scoresHtml;
+
+scoresHtml =‘<div class=“highscore”><div class=“initials”>dink</div><div class=“score”>500</div></div>”; 
+*/
+
+
+
+
+
+
+
+
+
+
+  }
+
+// function for saving highscore
+function saveHighscore() {
+    // get value of input box
+    var initials = initialsEl.value.trim();
+  
+    // make sure value wasn't empty
+    if (initials !== "") {
+      // get saved scores from localstorage, or if not any, set to empty array
+      var highscores =
+        JSON.parse(window.localStorage.getItem("highscores")) || [];
+  
+      // format new score object for current user
+      var newScore = {
+        score: time,
+        initials: initials
+      };
+  
+      // save to localstorage
+      highscores.push(newScore);
+      window.localStorage.setItem("highscores", JSON.stringify(highscores));
+  
+      // redirect to next page
+      window.location.href = "highScore.html";
+    }
+  }
+
+  function checkForEnter(event) {
+    // "13" represents the enter key
+    if (event.key === "Enter") {
+      saveHighscore();
+    }
+  }
+  
+  // user clicks button to submit initials
+  submitBtn.onclick = saveHighscore;
+  
+  // user clicks button to start quiz
+  startBtn.onclick = startQuiz;
+  
+  initialsEl.onkeyup = checkForEnter;
+
+
+
+  function printHighscores() {
+    // either get scores from localstorage or set to empty array
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+  
+    // sort highscores by score property in descending order
+    highscores.sort(function(a, b) {
+      return b.score - a.score;
+    });
+  
+    highscores.forEach(function(score) {
+      // create li tag for each high score
+      var liTag = document.createElement("li");
+      liTag.textContent = score.initials + " - " + score.score;
+  
+      // display on page
+      var olEl = document.getElementById("highscores");
+      olEl.appendChild(liTag);
+    });
+  }
+  
+  function clearHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+  }
+  
+  document.getElementById("clear").onclick = clearHighscores;
+  
+  // run function when page loads
+  printHighscores();*/
