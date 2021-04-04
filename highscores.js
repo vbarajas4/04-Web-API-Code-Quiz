fcaa03c588d4fadfda38bc57705079a122d03ab2
Vanessa Maldonado <vbarajas4@gmail.com>
2021-04-03 19:11:10 -0700

    finished troubleshooting highscores js

--- highscores.js
+++ highscores.js
@@ -1,4 +1,11 @@
-var data = JSON.parse(window.localStorage.getItem("highscore"));
+
+//get scores from localstorage or set to empty array
+var data = JSON.parse(window.localStorage.getItem("highscore")) || [];
+
+// sort highscores by score property from highest to lowerst.
+data.sort(function(a, b) {
+  return b.score - a.score;
+});
 
 data.forEach(element => {
     var tableEl = document.createElement("tr");
@@ -13,94 +20,10 @@ data.forEach(element => {
     document.getElementById("highscorestable").appendChild(tableEl);
 });
 
+function clearHighscores() {
+  window.localStorage.removeItem("highscore");
+  window.location.reload();
+}
 
+document.getElementById("clear-btn").onclick = clearHighscores;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// function for saving highscore
-function saveHighscore() {
-    // get value of input box
-    var initials = initialsEl.value.trim();
-  
-    // make sure value wasn't empty
-    if (initials !== "") {
-      // get saved scores from localstorage, or if not any, set to empty array
-      var highscores =
-        JSON.parse(window.localStorage.getItem("highscore")) || [];
-  
-      // format new score object for current user
-      var newScore = {
-        score: time,
-        initials: initials
-      };
-  
-      // save to localstorage
-      highscores.push(newScore);
-      window.localStorage.setItem("highscores", JSON.stringify(highscores));
-  
-      // redirect to next page
-      window.location.href = "highScore.html";
-    }
-  }
-
-  function checkForEnter(event) {
-    // "13" represents the enter key
-    if (event.key === "Enter") {
-      saveHighscore();
-    }
-  }
-  
-  // user clicks button to submit initials
-  submitBtn.onclick = saveHighscore;
-  
-  // user clicks button to start quiz
-  startBtn.onclick = startQuiz;
-  
-  initialsEl.onkeyup = checkForEnter;
-
-
-
-  function printHighscores() {
-    // either get scores from localstorage or set to empty array
-    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
-  
-    // sort highscores by score property in descending order
-    highscores.sort(function(a, b) {
-      return b.score - a.score;
-    });
-  
-    highscores.forEach(function(score) {
-      // create li tag for each high score
-      var liTag = document.createElement("li");
-      liTag.textContent = score.initials + " - " + score.score;
-  
-      // display on page
-      var olEl = document.getElementById("highscores");
-      olEl.appendChild(liTag);
-    });
-  }
-  
-  function clearHighscores() {
-    window.localStorage.removeItem("highscores");
-    window.location.reload();
-  }
-  
-  document.getElementById("clear").onclick = clearHighscores;
-  
-  // run function when page loads
-  printHighscores();*/
